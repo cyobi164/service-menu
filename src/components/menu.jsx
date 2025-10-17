3bd188d1c65d498498dee5a760f69bf75c98df2c
Anchal <mrgnchl090@gmail.com>
2025-10-17 23:11:56 +0900

    saved

---
+++ src/components/menu.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Menu() {
+  return (
+    <div>
+      <h1>Welcome to My Menu</h1>
+      <p>This is your custom component.</p>
+    </div>
+  )
+}
+
+export default Menu
